e3b726cb9a17f44641e2c38e4262bd1c64cdbc63
arthurmelton <amtitan@icloud.com>
2021-09-27 20:53:37 -0400

    what I got

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use bevy::prelude::*;
 use rand::Rng;
 use bevy::input::mouse::MouseButtonInput;
 use bevy::app::Events;
+use bevy::ui::Val::Px;
+use std::borrow::{Borrow, BorrowMut};
 
 fn main() {
     App::build()
@@ -13,6 +15,8 @@ fn main() {
         .insert_resource(Start { bombs: 10, tiles_x: 5, tiles_y: 5, bounds: Vec2::new(500.0, 500.0) })
         .add_startup_system(startup.system())
         .add_system(handle_mouse_clicks.system())
+        .add_system(text_system.system())
+        .add_system(img_place.system())
         .run();
 }
 
@@ -29,6 +33,10 @@ struct Start {
     bounds:Vec2,
 }
 
+struct Img {
+    index:i32,
+}
+
 fn startup(mut commands: Commands, start: Res<Start>, mut materials: ResMut<Assets<ColorMaterial>>) {
     let mut rng = rand::thread_rng();
     let bounds = start.bounds;
@@ -38,13 +46,13 @@ fn startup(mut commands: Commands, start: Res<Start>, mut materials: ResMut<Asse
     let mut bombs = 0;
     for x in 0..start.tiles_x {
         for y in 0..start.tiles_y {
-            let mut bomb = ((start.bombs-bombs) as f32 / ((start.tiles_y*start.tiles_x)-index) as f32) > rng.gen::<f32>();
+            let bomb = ((start.bombs-bombs) as f32 / ((start.tiles_y*start.tiles_x)-index) as f32) > rng.gen::<f32>();
             if bomb {
                 bombs += 1;
             }
             commands
                 .spawn_bundle(SpriteBundle {
-                    material: materials.add(Color::rgb(0.0 + (((x+y)%2) as f32 * 255.0),0.0 + (((x+y)%2) as f32 * 255.0),0.0 + (((x+y)%2) as f32 * 255.0)).into()),
+                    material: materials.add(Color::rgb(0.5 + (((x+y)%2) as f32 * 0.1),0.5 + (((x+y)%2) as f32 * 0.1),0.5 + (((x+y)%2) as f32 * 0.1)).into()),
                     transform: Transform::from_xyz((bounds.x / 2.0) - (x as f32 * (bounds.x / start.tiles_x as f32)) - ((bounds.x/start.tiles_x as f32)/2.0), (bounds.y / 2.0) - (y as f32 * (bounds.y / start.tiles_y as f32)) - ((bounds.y/start.tiles_y as f32)/2.0), 0.0),
                     sprite: Sprite::new(Vec2::new(bounds.x/start.tiles_x as f32, bounds.y/start.tiles_y as f32)),
                     ..Default::default()
@@ -55,27 +63,158 @@ fn startup(mut commands: Commands, start: Res<Start>, mut materials: ResMut<Asse
     }
 }
 
-fn handle_mouse_clicks(mouse_input: Res<Input<MouseButton>>, windows: Res<Windows>, start: Res<Start>, mut query: Query<(&mut Tile)>) {
+fn handle_mouse_clicks(mouse_input: Res<Input<MouseButton>>, mut windows: ResMut<Windows>, start: Res<Start>, mut query: Query<(&mut Tile, Entity)>, mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>, asset_server: Res<AssetServer>,) {
     let win = windows.get_primary().expect("no primary window");
     if mouse_input.just_pressed(MouseButton::Left) || mouse_input.just_pressed(MouseButton::Right) {
         let pressed_left =  mouse_input.just_pressed(MouseButton::Left);
         let pos = Vec2::new(((win.cursor_position().unwrap().x - win.width()/2.0 + (start.bounds.x / 2.0))/(start.bounds.x / start.tiles_x as f32)).floor(), ((win.cursor_position().unwrap().y - win.height()/2.0 + (start.bounds.y / 2.0))/(start.bounds.y / start.tiles_y as f32)).floor());
         let index = ((start.tiles_y) as f32 - pos.y -1.0) + (start.tiles_y as f32 * ((start.tiles_x) as f32 - pos.x - 1.0));
-        for mut x in query.iter_mut() {
-            if x.index as f32 == index && !x.shown {
-                if pressed_left {
-                    let shown =  &mut x.shown;
-                    if *shown {
-                        *shown = false;
-                    }
-                    else {
-                        *shown = true;
-                    }
-                    println!("{}", index);
-                }
-            }
+        let mut query_var = query.iter_mut();
+        println!("{}", index);
+        let query_var_var = query_var.borrow_mut().nth(index as usize).unwrap();
+        let x = query_var_var.0;
+        let y = query_var_var.1;
+        if !x.shown && pressed_left {
+            pressed_left_fn(commands, windows, index as i32, y, start, materials, query, asset_server);
+        }
+        else if pressed_left {
+            let window = windows.get_primary_mut().unwrap();
+            commands.spawn_bundle(TextBundle {
+                text: Text {
+                    sections: vec![
+                        TextSection {
+                            value: "You Lost".to_string(),
+                            style: TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 40.0,
+                                color: Color::rgb(0.0, 0.0, 0.0),
+                            },
+                        },
+                    ],
+                    ..Default::default()
+                },
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    position: Rect {
+                        top: Px(window.height() / 2.0),
+                        left: Px(window.width() / 2.0),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                node: Node {
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
+        }
+        else if !pressed_left {
+            let yy = index%start.tiles_x;
+            let xx = (index as f32/start.tiles_x as f32).floor();
+            let bounds = start.bounds;
+            commands
+                .spawn_bundle(SpriteBundle {
+                    material: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+                    transform: Transform::from_xyz((bounds.x / 2.0) - (xx as f32 * (bounds.x / start.tiles_x as f32)) - ((bounds.x / start.tiles_x as f32) / 2.0), (bounds.y / 2.0) - (yy as f32 * (bounds.y / start.tiles_y as f32)) - ((bounds.y / start.tiles_y as f32) / 2.0), 0.0),
+                    sprite: Sprite::new(Vec2::new(bounds.x / start.tiles_x as f32, bounds.y / start.tiles_y as f32)),
+                    ..Default::default()
+                })
+                .insert(Tile { index: index, bomb: false, shown: true });
+            commands.entity(y).despawn();
         }
     }
 }
 
+fn text_system(mut windows: ResMut<Windows>, mut query: Query<(&mut Style, &mut Node, &Text)>) {
+    let mut query_var = query.iter_mut();
+    for mut x in query_var.borrow_mut() {
+        if x.borrow_mut().2.sections[0].value == format!("You Lost") {
+            let window = windows.get_primary_mut().unwrap();
+            x.borrow_mut().0.position.top = Px((window.height() / 2.0) - (x.borrow_mut().1.size.y / 2.0));
+            x.borrow_mut().0.position.left = Px((window.width() / 2.0) - (x.borrow_mut().1.size.x / 2.0));
+        }
+    }
+}
 
+fn img_place(mut windows: ResMut<Windows>, mut query: Query<(&mut Style, &mut Node, &Img)>, start: Res<Start>) {
+    for mut x in query.iter_mut() {
+        let bounds = start.bounds;
+        let yy = (start.tiles_y - 1) - (x.2.index%start.tiles_x);
+        let xx = (x.2.index as f32/start.tiles_x as f32).floor();
+        let window = windows.get_primary_mut().unwrap();
+        x.0.position.left = Px(((bounds.x / 2.0) - (xx as f32 * (bounds.x / start.tiles_x as f32)) - ((bounds.x / start.tiles_x as f32) / 2.0) + window.width() / 2.0) - x.1.size.x / 2.0);
+        x.0.position.top = Px(((bounds.y / 2.0) - (yy as f32 * (bounds.y / start.tiles_y as f32)) - ((bounds.y / start.tiles_y as f32) / 2.0) + window.height() / 2.0) - x.1.size.y / 2.0);
+    }
+}
+
+fn pressed_left_fn(mut commands:Commands, mut windows:ResMut<Windows>, index: i32, y: bevy::prelude::Entity, start:Res<Start>, mut materials: ResMut<Assets<ColorMaterial>>, mut query: Query<(&mut Tile, Entity)>, asset_server: Res<AssetServer>) {
+    let mut query_var = query.iter_mut();
+    let yy = index%start.tiles_x;
+    let xx = (index as f32/start.tiles_x as f32).floor();
+    let bounds = start.bounds;
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+            transform: Transform::from_xyz((bounds.x / 2.0) - (xx as f32 * (bounds.x / start.tiles_x as f32)) - ((bounds.x / start.tiles_x as f32) / 2.0), (bounds.y / 2.0) - (yy as f32 * (bounds.y / start.tiles_y as f32)) - ((bounds.y / start.tiles_y as f32) / 2.0), 0.0),
+            sprite: Sprite::new(Vec2::new(bounds.x / start.tiles_x as f32, bounds.y / start.tiles_y as f32)),
+            ..Default::default()
+        })
+        .insert(Tile { index: index, bomb: false, shown: true });
+    let window = windows.get_primary_mut().unwrap();
+    let mut number_of_bombs_near = 0;
+    if index - start.tiles_y >= 0 && index - start.tiles_y < start.tiles_x * start.tiles_y && query_var.borrow_mut().nth((index - start.tiles_y) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if index + start.tiles_y >= 0 && index + start.tiles_y < start.tiles_x * start.tiles_y && query_var.borrow_mut().nth((index + start.tiles_y) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if ((index + 1) as f32).floor() == ((index) as f32).floor() && query_var.borrow_mut().nth((index + 1) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if ((index - 1) as f32).floor() == ((index) as f32).floor() && query_var.borrow_mut().nth((index - 1) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if ((index + start.tiles_y + 1) as f32).floor() == ((index) as f32).floor() + 1.0 && query_var.borrow_mut().nth((index + start.tiles_y + 1) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if ((index + start.tiles_y - 1) as f32).floor() == ((index) as f32).floor() + 1.0 && query_var.borrow_mut().nth((index + start.tiles_y - 1) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if ((index - start.tiles_y + 1) as f32).floor() == ((index) as f32).floor() - 1.0 && query_var.borrow_mut().nth((index - start.tiles_y + 1) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if ((index - start.tiles_y - 1) as f32).floor() == ((index) as f32).floor() - 1.0 && query_var.borrow_mut().nth((index - start.tiles_y - 1) as usize).unwrap().0.bomb {
+        number_of_bombs_near += 1;
+    }
+    if number_of_bombs_near != 0 {
+        commands.spawn_bundle(TextBundle {
+            text: Text {
+                sections: vec![
+                    TextSection {
+                        value: number_of_bombs_near.to_string(),
+                        style: TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(1.0, 0.0, 0.0),
+                        },
+                    },
+                ],
+                ..Default::default()
+            },
+            style: Style {
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    top: Px(window.height() / 2.0),
+                    left: Px(window.width() / 2.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            node: Node {
+                ..Default::default()
+            },
+            ..Default::default()
+        }).insert(Img { index: index });
+    }
+    commands.entity(y).despawn();
+}
